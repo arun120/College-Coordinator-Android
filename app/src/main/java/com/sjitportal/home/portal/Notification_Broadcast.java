c8c03d1c7974a515ae2484bb3d91fda8d6558a69
Arun <n.arun120@gmail.com>
2016-11-11 18:52:46 +0530

    AlarmManager

--- app/src/main/java/com/sjitportal/home/portal/Notification_Broadcast.java
+++ app/src/main/java/com/sjitportal/home/portal/Notification_Broadcast.java
@@ -1,8 +1,11 @@
 package com.sjitportal.home.portal;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.SystemClock;
 import android.util.Log;
 
 public class Notification_Broadcast extends BroadcastReceiver {
@@ -24,6 +27,28 @@ public class Notification_Broadcast extends BroadcastReceiver {
         Intent intent1=new Intent(context,Notification_Alert.class);
         intent1.putExtra("rollno",b[0].getRollno());
         intent1.putExtra("Usr",b[0].getName());
-        context.startService(intent1);
+        //context.startService(intent1);
+
+
+
+        AlarmManager am = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        Intent i = new Intent(context, Notification_Alert.class);
+        i.putExtra("rollno",b[0].getRollno());
+        i.putExtra("Usr",b[0].getName());
+        PendingIntent pi = PendingIntent.getService(context, 0, i, 0);
+
+        am.cancel(pi);
+
+
+            am.setInexactRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                    SystemClock.elapsedRealtime() + 5*1000,//5 sec
+                    2*60*60*1000, pi);//2 hrs
+
+
+
+
     }
+
+
+
 }
