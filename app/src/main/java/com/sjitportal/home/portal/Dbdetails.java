a3eae7f0e100cc7c48c21aad6872a3037c92876b
arun120 <n.arun120@gmail.com>
2017-08-18 14:13:56 +0530

    Encrtption android 7

--- app/src/main/java/com/sjitportal/home/portal/Dbdetails.java
+++ app/src/main/java/com/sjitportal/home/portal/Dbdetails.java
@@ -3,8 +3,13 @@ package com.sjitportal.home.portal;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.util.Base64;
+import android.util.Log;
 
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.ByteBuffer;
 
 /**
  * Created by Home on 18-08-2015.
@@ -14,11 +19,28 @@ public class Dbdetails {
 static {
 
     Bitmap bm = BitmapFactory.decodeResource(StaticApp.getContext().getResources(),R.drawable.db);
+    try {
+        InputStream is = StaticApp.getContext().getAssets().open("db.jpg");
+        InputStreamReader reader=new InputStreamReader(is);
+        byte[] b=new byte[4096];
+        is.read(b);
+
+        String enc="";
+        for(byte bb:b)
+            enc+=(char)bb;
+
+        Log.i("Code Decrypted 1", enc);
+
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
+
     ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
     bm.compress(Bitmap.CompressFormat.JPEG, 0, baos);
     byte[] b = baos.toByteArray();
     String a= Base64.encodeToString(b, Base64.DEFAULT);
-    //Log.i("Code",a.substring(250,260)+"    "+a.substring(310,320));
+
 //    Toast.makeText(StaticApp.getContext(),a.substring(250,260)+"    "+a.substring(310,320),Toast.LENGTH_LONG).show();
     UserName="Android";
     Pass=a.substring(250,260)+a.substring(310,320);
